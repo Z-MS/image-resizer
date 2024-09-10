a8bc5a5fb3ff2931bdaf83ee8476fb52c3890d30
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-09-10 11:40:24 +0100

    Resizing images based on size

--- src/main/java/com/zms/image/ImageResizer.java
+++ src/main/java/com/zms/image/ImageResizer.java
@@ -14,6 +14,9 @@ import java.awt.image.BufferedImage;
 import org.imgscalr.Scalr;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryIteratorException;
@@ -21,6 +24,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class ImageResizer extends Application {
     @Override
@@ -43,8 +47,12 @@ public class ImageResizer extends Application {
             folder = directoryChooser.showDialog(stage);
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(folder.toPath())) {
                 for(Path file: stream) {
-                    // add file name to array
-                    files.add(file.toFile());
+                    // check if image's width is > 1000. If so, add to files array
+                    File imageFile = file.toFile();
+                    if(getWidth(imageFile) < 1000) {
+                        // add file to array
+                        files.add(imageFile);
+                    }
                 }
             } catch (IOException | DirectoryIteratorException ex) {
                 throw new RuntimeException(ex);
@@ -58,7 +66,9 @@ public class ImageResizer extends Application {
                 // 3. Delete files without appendage in them 4.Rename files by removing appendage
                 for(File file: files) {
                     BufferedImage resizedImage = simpleResizeImage(ImageIO.read(file), 1000);
-                    saveImage(resizedImage, "png", file.getName() + "_resized");
+                    String fullFilename = file.getName();
+                    String filenameWithoutExtension = fullFilename.substring(0, fullFilename.indexOf('.'));
+                    saveImage(resizedImage, filenameWithoutExtension + "_resized");
                 }
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
@@ -79,16 +89,31 @@ public class ImageResizer extends Application {
         return Scalr.resize(originalImage, targetWidth);
     }
 
-    void saveImage(BufferedImage image, String formatName, String filename) {
+    void saveImage(BufferedImage image, String filename) {
         try {
-            File outputFile = new File( "C:/Users/zayya/Pictures/" + filename + "." + formatName);
-            ImageIO.write(image, formatName, outputFile);
+            File outputFile = new File( "C:/Users/zayya/Pictures/AI/" + filename + ".png");
+            ImageIO.write(image, "png", outputFile);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
-    public static void resizeImage(File directory) throws IOException {
-
+    static int getWidth(final File image) throws IOException {
+        // gotten from StackOverflow and refined by Claude
+        int width;
+        try (ImageInputStream stream = new FileImageInputStream(image)) {
+            ImageReader reader = ImageIO.getImageReadersByFormatName("png").next();
+            try {
+                reader.setInput(stream);
+                width = reader.getWidth(0);
+            } finally {
+                reader.dispose();
+            }
+        } catch (IOException ex) {
+            throw new IOException("Error reading image dimensions: " + ex.getMessage());
+        } catch (NoSuchElementException ex) {
+            throw new NoSuchElementException("No PNG reader found");
+        }
+        return width;
     }
 
 }
